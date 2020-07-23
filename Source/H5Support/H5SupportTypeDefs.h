dec89fd1ac59c3acd04cf66ff88da9c24f5f8c90
Michael Jackson <mike.jackson@bluequartz.net>
2020-07-23 17:28:31 -0400

    Use the header file instead of defining the alias for hid_t
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

---
+++ Source/H5Support/H5SupportTypeDefs.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+
+/**
+ * @brief This used as a way to not have to include <hdf5.h> in a header file if
+ * the code is just using hid_t.
+ */
+using hid_t = int64_t;
+
